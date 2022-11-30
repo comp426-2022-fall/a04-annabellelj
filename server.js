9cba606bc1bff4c7aeec8bb9313547c44269bee2
Annabelle Jiang <annlin@email.unc.edu>
2022-11-30 10:11:16 -0500

    a

--- server.js
+++ server.js
@@ -11,32 +11,32 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/app/',(req,res) => {
-    res.send.status("200 OK");
+    res.send("200 OK");
 });
 
 app.get("app/roll",(req,res) => {
-    res.send(console.log(roll(6,2,1)));
+    res.send((roll(6,2,1)));
 });
 
-app.get('/app/roll/',(req,res) => {
+app.post('/app/roll/',(req,res) => {
     res.send(console.log(roll(parseInt(req.body.sides),parseInt(req.body.dice),parseInt(req.body.rolls))));
 });
 
 app.get('/app/roll/:sides/',(req,res) => {
-    res.status(200).send(roll(parseInt(req.params.sides),2,1));
+    res.send(roll(parseInt(req.params.sides),2,1));
 });
 
 app.get('/app/roll/:sides/:dice/',(req,res) => {
-    res.status(200).send(roll(parseInt(req.params.dice),1));
+    res.send(roll(parseInt(req.params.sides),parseInt(req.params.dice),1));
 });
 
 app.get('/app/roll/:sides/:dice/:rolls',(req,res) => {
-    res.send(console.log(roll(parseInt(req.params.sides),roll(parseInt(req.params.dice),roll(parseInt(req.params.rolls))))));
+    res.send(roll(parseInt(req.params.sides),roll(parseInt(req.params.dice),roll(parseInt(req.params.rolls)))));
 });
 
 
 
-app.use('*',(req,res) => {
+app.use((req,res) => {
     res.status(404).send('404 NOT FOUND');
 });
 
