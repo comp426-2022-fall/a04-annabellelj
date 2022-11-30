3a59fbf656d6d1695571b0a1a41977c7412ea62f
Annabelle Jiang <annlin@email.unc.edu>
2022-11-30 09:45:49 -0500

    a

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/app/',(req,res) => {
     res.send("200 OK");
 });
 
-app.get('/app/roll',(req,res) => {
+app.get("app/roll",(req,res) => {
     res.send(roll(6,2,1));
 });
 
@@ -30,10 +30,11 @@ app.get('/app/roll/:sides/:dice/',(req,res) => {
     res.send(roll(parseInt(req.params.dice),1));
 });
 
+app.get('/app/roll/:sides/:dice/:rolls',(req,res) => {
+    res.send(roll(parseInt(req.params.sides),roll(parseInt(req.params.dice),roll(parseInt(req.params.rolls)));
+});
 
-//app.get('/app/roll/:sides/:dice/:rolls/',(req,res) => {
-//    res.send(roll(parseInt(req.params.sides),roll(parseInt(req.params.dice),roll(parseInt(req.params.rolls)));
-//});
+
 
 app.get('*',(req,res) => {
     res.status(404).send('404 NOT FOUND');
